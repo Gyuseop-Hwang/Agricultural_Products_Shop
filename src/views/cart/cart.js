2cfe29cce031b13beffdd9ca6e13da40568b43d9
Jiyeong Jeong <8533283@naver.com>
2022-11-01 14:47:33 +0900

    Feat : 장바구니 선택항목 삭제 기능 추가 장바구니 선택항목 삭제 기능 추가 정지영

--- src/views/cart/cart.js
+++ src/views/cart/cart.js
@@ -2,64 +2,54 @@ const cartProductList = document.getElementById("cartProductList");
 const deleteSelectedButton = document.getElementById("deleteSelectedButton");
 const deleteAllButton = document.getElementById("deleteAllButton");
 
-let products = localStorage.getItem("products");
-let isCheckedArray = [];
+let products = [];
 
-function saveProduct() {
-  localStorage.setItem("products", JSON.stringify(products));
-
-  // backend로 요청 보내기
+function saveProduct(newProducts) {
+  localStorage.setItem("products", JSON.stringify(newProducts));
 }
 
-function deleteProduct() {
-  deleteProduct = isCheckedArray.map((ele) => {
-    return ele.id;
-  });
-  //삭제 코드 구현해야 함
+function deleteSelectedProduct() {
+  const checkBoxes = document.querySelectorAll(".checkBox");
+  let newProducts = [];
 
-  saveProduct();
+  // dom에서 선택항목 삭제
+  for (let i = 0; i < checkBoxes.length; i++) {
+    if (checkBoxes[i].checked === true) {
+      checkBoxes[i].parentElement.parentElement.remove();
+    }
+  }
+
+  // localstorage에 선택항목 삭제된 배열 저장
+  newProducts = products.filter((product) => {
+    for (let i = 0; i < checkBoxes.length; i++) {
+      if (checkBoxes[i].checked !== true && checkBoxes[i].id === product.id) {
+        return true;
+      }
+    }
+  });
+
+  saveProduct(newProducts);
 }
 
 function paintProduct(product) {
   const tr = document.createElement("tr");
-
-  tr.innerHTML = `<td>
-  <input type="checkbox" class="checkBox" id="${product.id}"/>
-  </td>
-  <td>${product.name}</td>
-  <td>${product.price}</td>
-  <td><input type="number" value="${product.count}" /></td>
+  tr.innerHTML = `<td><input type="checkbox" class="checkBox" id="${product.id}"/></td>
+    <td>${product.name}</td>
+    <td>${product.price}</td>
+    <td><input type="number" value="${product.count}" /></td>
   `;
-
   cartProductList.appendChild(tr);
 }
 
-deleteSelectedButton.addEventListener("click", deleteProduct);
-
-saveProduct();
+deleteSelectedButton.addEventListener("click", deleteSelectedProduct);
 
 // localStorage에 저장된 상품이 있을 때
-if (products) {
-  const parsedProducts = JSON.parse(products);
-  products = parsedProducts;
+const savedProducts = localStorage.getItem("products");
+if (savedProducts) {
+  products = JSON.parse(savedProducts);
   products.forEach(paintProduct);
 }
 
 // 장바구니 비어있을 때
 if (products.length < 1) {
 }
-
-// check된 product의 id 가져오기
-const checkBoxes = document.querySelectorAll(".checkBox");
-
-checkBoxes.forEach((checkBox) => {
-  checkBox.addEventListener("change", () => {
-    if (checkBox.checked) {
-      isCheckedArray.push(checkBox);
-    }
-    isCheckedArray = isCheckedArray.filter(
-      (checkBox) => checkBox.checked === true
-    );
-    console.log(isCheckedArray);
-  });
-});
