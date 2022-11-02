e1f92b9a1b1e6aefd0c02ccc9e5145054923e028
Jiyeong Jeong <8533283@naver.com>
2022-11-02 14:06:47 +0900

    Design : 주문페이지 css 주문페이지 css 대략적으로 적용 정지영

--- src/views/order/order.js
+++ src/views/order/order.js
@@ -1,2 +1,28 @@
-const name = document.getElementById("name");
+const recipient = document.getElementById("name");
 const postcode = document.getElementById("sample3_postcode");
+const address = document.getElementById("sample3_address");
+const detailAddress = document.getElementById("sample3_detailAddress");
+const extraAddress = document.getElementById("sample3_extraAddress");
+const firstPhoneNumber = document.getElementById("firstPhoneNumber");
+const middlePhoneNumber = document.getElementById("middlePhoneNumber");
+const lastPhoneNumber = document.getElementById("lastPhoneNumber");
+const payButton = document.getElementById("payButton");
+
+function getOrderInfo() {
+  let products = JSON.parse(localStorage.getItem("products"));
+  products = products.map((product) => {
+    return { productId: product.id, quantity: product.quantity };
+  });
+  console.log(products);
+  const orderInfo = {
+    recipient: recipient.value, //받는 사람
+    phoneNumber: `${firstPhoneNumber.value}-${middlePhoneNumber.value}-${lastPhoneNumber.value}`, // 전화번호
+    shippingAddress: `${postcode.value} ${address.value} ${detailAddress.value} ${extraAddress.value}`, // 주소
+    products, // 상품
+    userId: "",
+  };
+
+  console.log(orderInfo);
+}
+
+payButton.addEventListener("click", getOrderInfo);
