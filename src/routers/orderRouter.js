115d265d1cd83b0e6eb0b13930f4e7234231afa8
hyeongyu shin <rkrkdldkd@gmail.com>
2022-11-02 16:33:28 +0900

    validation 임시 커밋

--- src/routers/orderRouter.js
+++ src/routers/orderRouter.js
@@ -2,90 +2,122 @@ import { Router } from 'express';
 import { orderService } from '../services';
 import { AppError } from '../utils';
 import { wrapAsync } from '../utils/wrapAsync';
+import Joi from 'joi';
 
 const orderRouter = Router();
 
-// 로그인 했는지 체크
+const postSchema = Joi.object({
+  recipient: Joi.string().required(),
+  phoneNumber: Joi.string().required(),
+  shippingAddress: Joi.string().required(),
+  products: Joi.array().items(
+    Joi.object({
+      productId: Joi.string().required(),
+      quantity: Joi.number().integer().min(1).required(),
+    })
+  ),
+  userId: Joi.string().required(),
+});
+
+const putSchema = Joi.object({
+  status: Joi.string(),
+  shippingAddress: Joi.string(),
+}).or('status', 'shippingAddress');
 
 orderRouter.get(
-    '/orders',
-    // 이 부분에서 관리자인지 권한 확인 필요
-    wrapAsync(async (req, res) => {
-        const orders = await orderService.findAllOrders({});
+  '/orders',
+  // 이 부분에서 관리자인지 권한 확인 필요
+  wrapAsync(async (req, res) => {
+    const orders = await orderService.findAllOrders({});
 
-        res.status(200).send(orders);
-    })
+    res.status(200).send(orders);
+  })
 );
 
 orderRouter.get(
-    '/orders/:userId',
-    wrapAsync(async (req, res) => {
-        const { userId } = req.params;
-        const orders = await orderService.findOrdersByUserId(userId);
+  '/orders/:userId',
+  wrapAsync(async (req, res) => {
+    const { userId } = req.params;
 
-        res.status(200).send(orders);
-    })
+    const orders = await orderService.findOrdersByUserId(userId);
+
+    res.status(200).send(orders);
+  })
 );
 
 orderRouter.post(
-    '/orders',
-    wrapAsync(async (req, res) => {
-        const { recipient, phoneNumber, products, userId, shippingAddress } =
-            req.body;
-        // 여러 유효성 검사
+  '/orders',
+  wrapAsync(async (req, res) => {
+    const { recipient, phoneNumber, products, userId, shippingAddress } =
+      req.body;
 
-        const totalPrice = await orderService.calculateTotalPrice(products);
+    const resultJoi = postSchema.validate({
+      recipient,
+      phoneNumber,
+      products,
+      userId,
+      shippingAddress,
+    });
 
-        const result = await orderService.addOrder({
-            recipient,
-            phoneNumber,
-            totalPrice,
-            products,
-            shippingAddress,
-            userId,
-        });
+    if (resultJoi.error) {
+      throw new AppError(400, '유효성 검사에서 실패했습니다!.');
+    }
 
-        res.status(201).send(result);
-    })
+    const totalPrice = await orderService.calculateTotalPrice(products);
+
+    const result = await orderService.addOrder({
+      recipient,
+      phoneNumber,
+      totalPrice,
+      products,
+      shippingAddress,
+      userId,
+    });
+
+    res.status(201).send(result);
+  })
 );
 
 orderRouter.put(
-    '/orders/:orderId',
-    wrapAsync(async (req, res) => {
-        const { orderId } = req.params;
-        const { status, shippingAddress } = req.body;
+  '/orders/:orderId',
+  wrapAsync(async (req, res) => {
+    const { orderId } = req.params;
+    const { status, shippingAddress } = req.body;
 
-        if (!status && !shippingAddress)
-            throw new AppError(400, '정보를 다 채워서 주세요!');
+    const result = putSchema.validate({ status, shippingAddress });
 
-        if (status) {
-            const updatedOrder = await orderService.updateOrderStatus(orderId, {
-                status,
-            });
+    if (result.error) {
+      throw new AppError(400, '정보를 다 채워서 주세요!');
+    }
 
-            return res.status(200).send(updatedOrder);
-        } else if (shippingAddress) {
-            const updatedOrder = await orderService.updateOrderShippingAddress(
-                orderId,
-                {
-                    shippingAddress,
-                }
-            );
-            return res.status(200).send(updatedOrder);
+    if (status) {
+      const updatedOrder = await orderService.updateOrderStatus(orderId, {
+        status,
+      });
+
+      return res.status(200).send(updatedOrder);
+    } else if (shippingAddress) {
+      const updatedOrder = await orderService.updateOrderShippingAddress(
+        orderId,
+        {
+          shippingAddress,
         }
+      );
+      return res.status(200).send(updatedOrder);
+    }
 
-        res.status(200).send(orders);
-    })
+    res.status(200).send(orders);
+  })
 );
 
 orderRouter.delete(
-    '/orders/:orderId',
-    wrapAsync(async (req, res) => {
-        const { orderId } = req.params;
-        const deletedOrder = await orderService.deleteOrder(orderId);
+  '/orders/:orderId',
+  wrapAsync(async (req, res) => {
+    const { orderId } = req.params;
+    const deletedOrder = await orderService.deleteOrder(orderId);
 
-        res.status(201).send(deletedOrder);
-    })
+    res.status(201).send(deletedOrder);
+  })
 );
 
 export { orderRouter };
--- src/services/orderService.js
+++ src/services/orderService.js
@@ -1,71 +1,72 @@
 import { orderModel } from '../db';
 import { productModel } from '../db';
+import { AppError } from '../utils';
 
 class OrderService {
-    constructor(orderModel) {
-        this.orderModel = orderModel;
-    }
+  constructor(orderModel) {
+    this.orderModel = orderModel;
+  }
 
-    async findAllOrders() {
-        const orders = await this.orderModel.findAll();
-        return orders;
-    }
+  async findAllOrders() {
+    const orders = await this.orderModel.findAll();
+    return orders;
+  }
 
-    async findOrdersByUserId(userId) {
-        const orders = await this.orderModel.findByUserId(userId);
-        return orders;
-    }
+  async findOrdersByUserId(userId) {
+    const orders = await this.orderModel.findByUserId(userId);
+    return orders;
+  }
 
-    async addOrder(orderInfo) {
-        const createdOrder = await this.orderModel.create(orderInfo);
-        if (!createdOrder)
-            throw new AppError(400, '등록 요청을 실행할 수 없습니다.');
+  async addOrder(orderInfo) {
+    const createdOrder = await this.orderModel.create(orderInfo);
+    if (!createdOrder)
+      throw new AppError(400, '등록 요청을 실행할 수 없습니다.');
 
-        return createdOrder;
-    }
+    return createdOrder;
+  }
 
-    async updateOrderStatus(orderId, orderStatus) {
-        const updatedOrder = await this.orderModel.update(orderId, orderStatus);
+  async updateOrderStatus(orderId, orderStatus) {
+    const updatedOrder = await this.orderModel.update(orderId, orderStatus);
 
-        if (!updatedOrder)
-            throw new AppError(400, '수정 요청을 실행할 수 없습니다.');
+    if (!updatedOrder)
+      throw new AppError(400, '수정 요청을 실행할 수 없습니다.');
 
-        return updatedOrder;
-    }
+    return updatedOrder;
+  }
 
-    async updateOrderShippingAddress(orderId, orderShippingAddress) {
-        const updatedOrder = await this.orderModel.update(
-            orderId,
-            orderShippingAddress
+  async updateOrderShippingAddress(orderId, orderShippingAddress) {
+    const updatedOrder = await this.orderModel.update(
+      orderId,
+      orderShippingAddress
+    );
+
+    if (!updatedOrder)
+      throw new AppError(400, '수정 요청을 실행할 수 없습니다.');
+
+    return updatedOrder;
+  }
+
+  async deleteOrder(orderId) {
+    const deletedOrder = await this.orderModel.delete(orderId);
+
+    if (!deletedOrder) throw new AppError(400, '삭제할 주문이 없습니다!');
+
+    return deletedOrder;
+  }
+
+  async calculateTotalPrice(requestedProducts) {
+    const allProducts = await productModel.findAllProducts();
+    const totalPrice = requestedProducts.reduce(
+      (acc, { productId, quantity }) => {
+        const pickedProduct = allProducts.find(
+          (v) => v._id.toString() === productId
         );
-
-        if (!updatedOrder)
-            throw new AppError(400, '수정 요청을 실행할 수 없습니다.');
-
-        return updatedOrder;
-    }
-
-    async deleteOrder(orderId) {
-        const deletedOrder = await this.orderModel.delete(orderId);
-
-        if (!deletedOrder) throw new AppError(400, '삭제할 주문이 없습니다!');
-
-        return deletedOrder;
-    }
-
-    async calculateTotalPrice(requestedProducts) {
-        const allProducts = await productModel.findAllProducts();
-        const totalPrice = requestedProducts.reduce(
-            (acc, { productId, quantity }) => {
-                const pickedProduct = allProducts.find(
-                    (v) => v._id.toString() === productId
-                );
-                return acc + pickedProduct.price * quantity;
-            },
-            0
-        );
-        return totalPrice;
-    }
+        return acc + pickedProduct.price * quantity;
+      },
+      0
+    );
+    return totalPrice;
+  }
 }
 
 const orderService = new OrderService(orderModel);
