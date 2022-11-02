a9f6c42336582427a979487ab349a18e8412fa18
승연 오 <hisy6887@gmail.com>
2022-11-02 21:05:49 +0900

    ADD 전체 가격 표시 수량 입력에 따는 전체 가격 바뀌는 것 추가 오승연

--- src/views/productDetail/productDetail.js
+++ src/views/productDetail/productDetail.js
@@ -6,7 +6,8 @@ const categoryNameSpan = document.getElementById("categoryName")
 const descriptionSpan = document.getElementById("discription")
 const addCartBtn = document.getElementById("addCartBtn");
 const countInput = document.getElementById("count");
-//json server에서 item object 가져오기
+const totalPriceSpan = document.getElementById("totalPrice");
+//json server에서 목데이터로 item object 가져오기
 function getProduct() {
 	return fetch("http://localhost:4000/item").then(res => res.json());
 }
@@ -22,10 +23,17 @@ async function printProduct() {
 	quantitySpan.innerText = `남은 수량: ${quantity}`
 	categoryNameSpan.innerText = category.name;
 	descriptionSpan.innerText = description;
+	//수량 변경 시 가격 변경
+	countInput.addEventListener("change", ex => {
+		totalPriceSpan.innerText = `${(Number(countInput.value) * price).toLocaleString()}원`
+	})
 	addCartBtn.addEventListener("click", (ev) => {
 		ev.preventDefault();
 		const count = countInput.value;
-		localStorage.setItem("product", JSON.stringify({ ...product, "count": count }))
+		localStorage.setItem("product", JSON.stringify({ ...product, "count": Number(count) }))
+		alert("장바구니에 추가되었습니다.");
 	})
+	totalPriceSpan.innerText = `${price.toLocaleString()}원`
+
 }
 printProduct();
