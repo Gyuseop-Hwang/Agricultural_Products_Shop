71bad75bbdcb45cd9608a02553ff3b01b67a7126
hyeongyu shin <rkrkdldkd@gmail.com>
2022-11-02 21:03:11 +0900

    dev에 merge 전 be merge

--- src/app.js
+++ src/app.js
@@ -1,14 +1,18 @@
-import cors from "cors";
-import express from "express";
-import { viewsRouter, userRouter, productRouter, orderRouter } from "./routers";
-import { authRequired, errorHandler, loginRequired, notFound } from "./middlewares";
-import { morgan } from 'morgan';
-
+import cors from 'cors';
+import express from 'express';
+import { viewsRouter, userRouter, productRouter, orderRouter } from './routers';
+import {
+  authRequired,
+  errorHandler,
+  loginRequired,
+  notFound,
+} from './middlewares';
+import morgan from 'morgan';
 
 const app = express();
 
 // log 기록
-app.use(morgan('dev'))
+app.use(morgan('dev'));
 
 // CORS 에러 방지
 app.use(cors());
@@ -29,11 +33,6 @@ app.use('/api', userRouter);
 app.use('/api', productRouter);
 // app.use('/api', loginRequired, authRequired, adminProductsRouter)
 app.use('/api', loginRequired, orderRouter);
-
-
-
-
-
 
 // 순서 중요 (errorHandler은 다른 일반 라우팅보다 나중에 있어야 함)
 // 그래야, 에러가 났을 때 next(error) 했을 때 여기로 오게 됨
