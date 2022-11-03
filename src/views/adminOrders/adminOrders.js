1d3c8f61d0b094948de8753b484c7ee42504da6b
younngg <djaak3283@gmail.com>
2022-11-03 20:57:25 +0900

    Fix : 관리자 주문내역 조회 페이지 수정 api 요청 코드 수정 updateOrderStatus 고쳐야함 (put 오류) 정지영

--- src/views/adminOrders/adminOrders.js
+++ src/views/adminOrders/adminOrders.js
@@ -3,54 +3,75 @@ const orderList = document.getElementById("orderList");
 
 // 삭제, 상태 수정 코드 추가해야 함
 // 받아온 주문내역 화면에 보여줌, ejs로 추후 수정
-function paintOrders(order) {
-  // const {
-  //   //id 받아와야됨
-  //   products,
-  //   phoneNumber,
-  //   recipient,
-  //   shippringAddress,
-  //   status,
-  //   totalPrice,
-  //   userId,
-  // } = order;
+function paintOrders(order, index) {
+  const {
+    _id,
+    products,
+    phoneNumber,
+    recipient,
+    shippingAddress,
+    status,
+    totalPrice,
+    userId,
+  } = order;
 
   const tr = document.createElement("tr");
   tr.className = "order";
   tr.innerHTML = `<td>2022/10/11</td>
-  <td>${userId}}</td>
+  <td>${userId}</td>
   <td>
   ${products
-    .map((product) => `<p>${product.product} * ${product.quantity}</p>`)
+    .map((product) => `<p>${product.productId} * ${product.quantity}</p>`)
     .join("")}
   </td>
-  <td>${totalPrice.toLocaleString()}원</td>
+  <td>{totalPrice.toLocaleString()}원</td>
   <td>
     <p>${recipient}</p>
-    <p>${shippringAddress}</p>
+    <p>${shippingAddress}</p>
     <p>${phoneNumber}</p>
   </td>
   <td>
-    <select class="select is-small" name="" id=""">
-      <option value="" selected>배송준비중</option>
+    <select class="select is-small" name="" id="statusSelect${index}">
+      <option value="" selected>주문 완료</option>
+      <option value="">배송준비중</option>
       <option value="">배송중</option>
       <option value="">배송완료</option>
     </select>
-    <button class="button is-small" id="modifyStatusBtn">수정</button>
+    <button class="button is-small" id="modifyStatusBtn${index}">수정</button>
   </td>
   <td>
-    <button class="button is-small" id="cancleOrderBtn">주문취소</button>
+    <button class="button is-small" id="cancleOrderBtn${index}">주문삭제</button>
   </td>`;
   orderList.appendChild(tr);
+
+  // 주문 삭제
+  const cancleOrderBtn = document.getElementById(`cancleOrderBtn${index}`);
+  cancleOrderBtn.addEventListener("click", () => {
+    deleteOrder(_id);
+    tr.remove();
+  });
+  // 배송 상태 변경
+  const modifyStatusBtn = document.getElementById(`modifyStatusBtn${index}`);
+  modifyStatusBtn.addEventListener("click", () => {
+    const selectedStatus = document.getElementById(`statusSelect${index}`);
+    updateOrderStatus(_id, { status: selectedStatus.value });
+  });
 }
 
 // 배송 상태 변경
 async function updateOrderStatus(id, status) {
   try {
-    const result = await Api.put(
-      `http://localhost:5500/api/orders/${id}`,
-      status
-    );
+    const bodyData = JSON.stringify(status);
+    const result = await fetch(`/api/admin/orders/${id}`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${sessionStorage.getItem("token")}`,
+      },
+      body: bodyData,
+    });
+
+    //Api.patch(`/api/admin/orders`, id, { status: status });
     console.log(result);
   } catch (err) {
     console.error(err.stack);
@@ -61,7 +82,7 @@ async function updateOrderStatus(id, status) {
 // 주문 삭제
 async function deleteOrder(id) {
   try {
-    const result = await Api.delete("http://localhost:5500/api/orders", id);
+    const result = await Api.delete("/api/orders", id);
     console.log(result);
   } catch (err) {
     console.error(err.stack);
@@ -72,8 +93,8 @@ async function deleteOrder(id) {
 // 전체 주문 내역 가져오기
 async function getOrders() {
   try {
-    const result = await Api.get("http://localhost:5500/api/orders");
-    console.log(result);
+    const result = await Api.get("/api/admin/orders");
+    result.forEach((order, index) => paintOrders(order, index));
   } catch (err) {
     console.error(err.stack);
     alert(`문제가 발생하였습니다. 확인 후 다시 시도해 주세요: ${err.message}`);
