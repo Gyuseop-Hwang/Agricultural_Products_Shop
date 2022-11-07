8de48e4c717cafa201f0d3f391a03deb70e6bb4f
younngg <djaak3283@gmail.com>
2022-11-07 20:20:13 +0900

    Design : adminProducts, cart, order 수정 디자인 적용 및 ejs 변환 정지영

--- src/public/js/adminProducts.js
+++ src/public/js/adminProducts.js
@@ -1,26 +1,12 @@
 import * as Api from "/api.js";
 
-const productTable = document.getElementById("productListTable");
-
-// 전체 상품 출력
-function printAllProducts(product, index) {
-  console.log(product);
-  const { _id: id, category, image, title, price, quantity } = product;
-  const tr = document.createElement("tr");
-  tr.innerHTML = `<tr>
-  <td>${category.name}</td>
-  <td><img class="product-image" src="${image.path}" /></td>
-  <td>${title}</td>
-  <td>${price}</td>
-  <td>${quantity}</td>
-  <td><a href="/adminProducts/${id}"><button class="button is-small">수정</button></a></td>
-  <td><button class="button is-small" id="deleteButton${index}">삭제</button></a></td>
-</tr>`;
-  productTable.appendChild(tr);
-
+// 삭제 버튼 이벤트 등록
+function clickDeleteButton(product, index) {
   const deleteButton = document.getElementById(`deleteButton${index}`);
+
+  const tr = document.getElementById(`tr${index}`);
   deleteButton.addEventListener("click", () => {
-    deleteProduct(id);
+    deleteProduct(product._id);
     tr.remove();
   });
 }
@@ -29,7 +15,7 @@ function printAllProducts(product, index) {
 async function getAllProducts() {
   try {
     const { categories, products } = await Api.get("/api/products");
-    products.forEach((product, index) => printAllProducts(product, index));
+    products.forEach((product, index) => clickDeleteButton(product, index));
   } catch (err) {
     console.error(err.stack);
     alert(`문제가 발생하였습니다. 확인 후 다시 시도해 주세요: ${err.message}`);
