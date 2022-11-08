fcb25145dc0bdc70dea2a3f4fa85b16deac936ad
승연 오 <hisy6887@gmail.com>
2022-11-09 00:38:05 +0900

    Fix viewRouter 수정 불필요한 async 제거 오승연

--- src/routers/viewsRouter.js
+++ src/routers/viewsRouter.js
@@ -10,10 +10,10 @@ const viewsRouter = express.Router();
 // 페이지별로 html, css, js 파일들을 라우팅함
 // 아래와 같이 하면, http://localhost:5000/ 에서는 views/home/home.html 파일을,
 // http://localhost:5000/register 에서는 views/register/register.html 파일을 화면에 띄움
-viewsRouter.use("/home", async (req, res, next) => {
+viewsRouter.use("/home", (req, res, next) => {
   res.render("home/home.ejs");
 });
-viewsRouter.use("/register", async (req, res, next) => {
+viewsRouter.use("/register", (req, res, next) => {
   res.render("register/register.ejs");
 });
 viewsRouter.use("/login", async (req, res, next) => {
