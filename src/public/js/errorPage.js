7903cc6d494fa93de013942131dc9be0ce98e096
승연 오 <hisy6887@gmail.com>
2022-11-10 19:01:14 +0900

    Fix 에러 수정 script 에러 수정 오승연

--- src/public/js/errorPage.js
+++
@@ -1,3 +0,0 @@
-const errorTitle = document.getElementById("errorTitle");
-const errorSubtitle = document.getElementById("errorSubtitle");
-const errorDescription = document.getElementById("errorDescription");
