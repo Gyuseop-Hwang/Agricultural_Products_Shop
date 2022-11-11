8be64d317e557fde987a8e7ae1bdf8aa421cbe40
hyeongyu shin <rkrkdldkd@gmail.com>
2022-11-11 11:42:21 +0900

    🔇 불필요한 log 및 코드 제거

--- src/middlewares/index.js
+++ src/middlewares/index.js
@@ -1,6 +1,3 @@
-// 나중에 import할 때 코드 길이가 짧아지도록 해 주고,
-// 이 middlewares 폴더의 파일들 및 export 되는 모듈들을 깔끔하게 묶어주는 역할임.
-
 export * from './errorHandler.js';
 export * from './loginRequired.js';
 export * from './authRequired.js';
