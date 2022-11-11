a82b459af831629191c39b4e6bdf0b4ec0315bb9
Jensen-Hwang <rbtjqghkd@gmail.com>
2022-11-11 11:54:25 +0900

    주석 정리

--- src/middlewares/errorHandler.js
+++ src/middlewares/errorHandler.js
@@ -6,11 +6,7 @@ function notFoundErrorHandler(req, res, next) {
 }
 
 function errorHandler(error, req, res, next) {
-  // 터미널에 노란색으로 출력됨.
   console.log('\x1b[33m%s\x1b[0m', error.stack);
-  const { statusCode = 500, message = 'Server-side Problem' } = error;
-
-  // res.status(statusCode).json({ result: "error", reason: message });
   app.locals.statusCode = error.statusCode;
   app.locals.message = error.message;
   res.redirect('/errorPage');
